0da363f49c32c3c40b62d7e8a86502b81a0e7351
Anushka Pandey <anushkapandey7002@gmail.com>
2026-07-23 15:20:11 +0530

    eventLoop

--- LAB 1/EventLoop/task1.js
+++ LAB 1/EventLoop/task1.js
@@ -1,27 +1,23 @@
-const f1 = () => { //crete three function f1 , f2, f3 and print their names
-    console.log("f1 starts");
-    f2();
-    console.log("f1 running");
-    console.log("f1 ends");
-}
-const f2 = () => {
-    console.log("f2 starts");
-    f3();
-    console.log("f2 running");
-    console.log("f2 ends");
-}
-const f3 = () => {
-    console.log("f3 starts");
-    console.log("f3 running");
-    console.log("f3 ends");
+const f1 = () => {
+  console.log("f1 starts");
+  f2();
+  console.log("f1 running");
+  console.log("f1 end");
 };
-function main(){
-    console.log("main starts");
-    f1();
-    console.log("main running");
-    console.log("main ends");
-    f2();
-    f3();
-    console.log("end main");
+const f2 = () => {
+  console.log("f2 starts");
+  f3();
+  console.log("f2 running");
+  console.log("f2 ends");
+};
+const f3 = () => {
+  console.log("f3 starts");
+  console.log("f3 running");
+  console.log("f3 end");
+};
+function main() {
+  console.log("main");
+  f1();
+  console.log("end main");
 }
 main();
